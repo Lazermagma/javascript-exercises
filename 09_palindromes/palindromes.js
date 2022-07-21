527066821b3cec10ac881dab95d3b62776023bcd
lazermagma <masterabdelrahman7@gmail.com>
2022-07-21 02:58:32 +0200

    answered nicely

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,51 @@
-const palindromes = function (str) {
 
-    let newStr = '';
+function reverse(str){
+let length = str.length;
+let reversed = '';
 
+     while (length >= 0) {
+        
+        reversed += str.charAt(length);
+         
+        length --;
+     }
+     
+     return  reversed ;
+
+
+}
+
+function caseInsensitive(str){
     
+    let UpperCase = str.toUpperCase();
 
+    return UpperCase ;
+
+}
+
+
+const palindromes = function (str) {
+   
+    str = caseInsensitive(str).replace(/[^A-Z]/g,"") ;
+    let newStr = caseInsensitive(reverse(str));
+  // newStr = newStr.replace(/[^A-Z]/,"");
+   if (str == newStr) 
+    {
+        return true ;
+    }
+
+    else { 
+        return false ;
+    }
+        
 };
+
+ 
+ 
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+

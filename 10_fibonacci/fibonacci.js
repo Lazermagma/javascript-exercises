8b2715b7a5562e27560e1e081c25aa3c3275b7e5
lazermagma <masterabdelrahman7@gmail.com>
2022-07-21 04:09:47 +0200

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,27 @@
-const fibonacci = function() {
+
+//fibonacci rule n = n-1 + n-2  or n = n1 + n2
+
+// the basic idea that i want to do is to translate the location into the value of fib 
+
+const fibonacci = function(location) {
+    
+   if (location < 0) return "OOPS";
+   if (location === 0) return 0;
+
+let n1 = 0;
+let n2 = 1;
+
+   for (let i = 1; i < location ; i++){
+         
+    const temp = n2 ;
+    n2 = n2 + n1 ;
+    n1 = temp;
+
+   }
+
+   return n2;
+   
+
 
 };
 
